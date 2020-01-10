adfd754fffb5a0a768902d08570fbbb433c1ce21
Amnesic Oblex <taylor@Taylors-MacBook-Pro.local>
2020-01-09 22:09:35 -0500

    Added race JSON file creator function for front ends.

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ files.forEach((file) => {
     var match = file.match(regex)[1]
     races[match] = JSON.parse(fs.readFileSync(Path.join(raceDir, file), 'utf8'))
 })
-
+// TODO: add human variant
 // Races concat function module export
-module.exports.races = races
+
+function createRaceJSON (path) {
+    fs.writeFile(path, races, () => {
+        console.log(`Race JSON created or updated at ${path}`)
+    })
+}
+
+module.exports = {
+    races: races,
+    createRaceJSON: createRaceJSON
+}
